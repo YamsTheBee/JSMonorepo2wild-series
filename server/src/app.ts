5588125d1852feab107c81ddc6e49c714a01c4c2
YamsTheBe <Mariamadiaw.a@hotmail.fr>
2025-03-04 15:22:20 +0100

    This is my first commit

--- server/src/app.ts
+++ server/src/app.ts
@@ -18,11 +18,11 @@ const app = express();
 // You should NOT do that: such code uses the `cors` module to allow all origins, which can pose security issues.
 // For this pedagogical template, the CORS code allows CLIENT_URL in development mode (when process.env.CLIENT_URL is defined).
 
-import cors from "cors";
+// import cors from "cors";
 
-if (process.env.CLIENT_URL != null) {
-  app.use(cors({ origin: [process.env.CLIENT_URL] }));
-}
+// if (process.env.CLIENT_URL != null) {
+//   app.use(cors({ origin: [process.env.CLIENT_URL] }));
+// }
 
 // If you need to allow extra origins, you can add something like this:
 
@@ -67,58 +67,58 @@ app.use(router);
 
 /* ************************************************************************* */
 
-// Production-ready setup: What is it for?
+// // Production-ready setup: What is it for?
 
-// The code includes sections to set up a production environment where the client and server are executed from the same processus.
+// // The code includes sections to set up a production environment where the client and server are executed from the same processus.
 
-// What it's for:
-// - Serving client static files from the server, which is useful when building a single-page application with React.
-// - Redirecting unhandled requests (e.g., all requests not matching a defined API route) to the client's index.html. This allows the client to handle client-side routing.
+// // What it's for:
+// // - Serving client static files from the server, which is useful when building a single-page application with React.
+// // - Redirecting unhandled requests (e.g., all requests not matching a defined API route) to the client's index.html. This allows the client to handle client-side routing.
 
-import fs from "node:fs";
-import path from "node:path";
+// import fs from "node:fs";
+// import path from "node:path";
 
-// Serve server resources
+// // Serve server resources
 
-const publicFolderPath = path.join(__dirname, "../../server/public");
+// const publicFolderPath = path.join(__dirname, "../../server/public");
 
-if (fs.existsSync(publicFolderPath)) {
-  app.use(express.static(publicFolderPath));
-}
+// if (fs.existsSync(publicFolderPath)) {
+//   app.use(express.static(publicFolderPath));
+// }
 
-// Serve client resources
+// // Serve client resources
 
-const clientBuildPath = path.join(__dirname, "../../client/dist");
+// const clientBuildPath = path.join(__dirname, "../../client/dist");
 
-if (fs.existsSync(clientBuildPath)) {
-  app.use(express.static(clientBuildPath));
+// if (fs.existsSync(clientBuildPath)) {
+//   app.use(express.static(clientBuildPath));
 
-  // Redirect unhandled requests to the client index file
+//   // Redirect unhandled requests to the client index file
 
-  app.get("*", (_, res) => {
-    res.sendFile("index.html", { root: clientBuildPath });
-  });
-}
+//   app.get("*", (_, res) => {
+//     res.sendFile("index.html", { root: clientBuildPath });
+//   });
+// }
 
-/* ************************************************************************* */
+// /* ************************************************************************* */
 
-// Middleware for Error Logging
-// Important: Error-handling middleware should be defined last, after other app.use() and routes calls.
+// // Middleware for Error Logging
+// // Important: Error-handling middleware should be defined last, after other app.use() and routes calls.
 
-import type { ErrorRequestHandler } from "express";
+// import type { ErrorRequestHandler } from "express";
 
-// Define a middleware function to log errors
-const logErrors: ErrorRequestHandler = (err, req, res, next) => {
-  // Log the error to the console for debugging purposes
-  console.error(err);
-  console.error("on req:", req.method, req.path);
+// // Define a middleware function to log errors
+// const logErrors: ErrorRequestHandler = (err, req, res, next) => {
+//   // Log the error to the console for debugging purposes
+//   console.error(err);
+//   console.error("on req:", req.method, req.path);
 
-  // Pass the error to the next middleware in the stack
-  next(err);
-};
+//   // Pass the error to the next middleware in the stack
+//   next(err);
+// };
 
-// Mount the logErrors middleware globally
-app.use(logErrors);
+// // Mount the logErrors middleware globally
+// app.use(logErrors);
 
 /* ************************************************************************* */
 
--- server/src/router.ts
+++ server/src/router.ts
@@ -8,10 +8,29 @@ const router = express.Router();
 
 // Define item-related routes
 import itemActions from "./modules/item/itemActions";
+// Declaration of a "Welcome" route
+
+import sayActions from "./module/sayActions";
 
 router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
+
+/* ************************************************************************* */
+// Declaration of a "Welcome" route
+
+import type { RequestHandler } from "express";
+
+const sayWelcome: RequestHandler = (req, res) => {
+  res.send("Welcome to Wild Series !");
+};
+router.get("/", sayWelcome);
+
+/* ************************************************************************* */
+// Define program-related routes
+import programActions from "./modules/program/programActions";
+
+router.get("/api/programs", programActions.browse);
 
 /* ************************************************************************* */
 
