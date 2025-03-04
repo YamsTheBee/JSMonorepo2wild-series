91bc9b1720fe3a20ca44e7ba20190934ee7ce816
YamsTheBe <Mariamadiaw.a@hotmail.fr>
2025-03-04 11:34:43 +0100

    this is my first commit

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,16 +1,27 @@
 // Load environment variables from .env file
+import express from "express";
 import "dotenv/config";
+import type { RequestHandler } from "express";
+
+// Fonction pour afficher le message de bienvenue
+const sayWelcome: RequestHandler = (req, res) => {
+  res.send("Welcome to Wild Series !");
+};
+// Initialiser l'application Express
+const app = express();
+
+// Ajouter la route GET /
+app.get("/", sayWelcome);
 
 // Check database connection
 // Note: This is optional and can be removed if the database connection
 // is not required when starting the application
 import "../database/checkConnection";
 
-// Import the Express application from ./app
-import app from "./app";
+// Import the Express application from ./aps
 
 // Get the port from the environment variables
-const port = process.env.APP_PORT;
+const port = process.env.APP_PORT || 3310;
 
 // Start the server and listen on the specified port
 app
